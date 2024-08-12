ee271e424e8c0004c018f1af31448f7054d36659
Kathiresan-M <146100703+Kathiresan-M@users.noreply.github.com>
2024-08-12 11:46:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,27 @@ import { createContext, useState} from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './components/Login.jsx';
 import { Register } from './components/Register.jsx';
+import { Home } from './components/Home.jsx';
+
 
 export const cartContext = createContext();
 
+// const backendUrl = "http://localhost:5000/";
+const backendUrl = "https://sqlserver-mk.onrender.com/";
 
 function App() { 
   const isLogin = window.localStorage.getItem("isLoggedIn");
   // conlose.log(isLogin);
   return (
-    <cartContext.Provider value={Data}>
+    <cartContext.Provider value={{Data,backendUrl}}>
     <BrowserRouter>
           <div className="main-container">
             <Routes>
-              <Route path='/' element={isLogin ? <SQLTopics /> : <Login />} />
+              <Route path='/' element={isLogin ? <Home /> : <Login />} />
+              {/* <Route path='/' element={isLogin ? <SQLTopics /> : <Login />} /> */}
               <Route path='/Login' element={<Login />} />
-              <Route path='/Register' element={isLogin ? <SQLTopics /> : <Register />} />
+              <Route path='/Home' element={<Home/>} />
+              <Route path='/Register' element={isLogin ? <Home /> : <Register />} />
               <Route path='/Registeration' element={<Register />} />
               <Route path='/SQLTopics' element={<SQLTopics />} />
               <Route path='/SqlTest' element={<SqlTest />} />
